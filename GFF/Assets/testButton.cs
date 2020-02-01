e0542593a01e6c8fb861a855aff46d59c9fac63c
kunitaka4649 <54517097+kunitaka4649@users.noreply.github.com>
2020-02-02 00:51:27 +0900

    ranking

---
+++ GFF/Assets/testButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testButton : MonoBehaviour
+{
+    public void onClicked(){
+        QuickRanking.Instance.SaveRanking("hoge", Random.Range(0, 1000));
+    }
+}
